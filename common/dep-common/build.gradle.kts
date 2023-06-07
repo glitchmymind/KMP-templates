b68100df2b1c6f72b46d26de75d2fba0b3d83aff
Pavel Lebakin <lebronkingsg@gmail.com>
2023-06-07 09:13:27 +0200

    [TASK-NUM] add common dependencies module

---
+++ common/dep-common/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("multiplatform-setup")
+    id("android-setup")
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(":common:core"))
+                implementation(project(":common:features:auth:domain"))
+            }
+        }
+    }
+}
---
+++ common/dep-common/src/commonMain/kotlin/PlatformSDK.kt
@@ -0,0 +1,30 @@
+import di.Inject
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.direct
+import org.kodein.di.singleton
+import config.PlatformConfig
+
+object PlatformSDK {
+
+    fun init(
+        configuration: PlatformConfig
+    ) {
+        val dependenciesModule = DI.Module(
+            name = "dependencies",
+            init = {
+                bind<PlatformConfig>() with singleton { configuration }
+            }
+        )
+
+        Inject.createDependencies(
+            DI {
+                importAll(
+                    dependenciesModule,
+                    coreModule,
+                    authRepositoryModule,
+                )
+            }.direct
+        )
+    }
+}
